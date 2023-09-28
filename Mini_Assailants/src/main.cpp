efc7a9de5389c3a12daa0599551558ab481177e7
GauravDe-git <gm.gauravde@gmail.com>
2023-09-28 13:14:03 +0530

    Feat: Added idle sprite anim, and fps overlay

--- Mini_Assailants/src/main.cpp
+++ Mini_Assailants/src/main.cpp
@@ -1,35 +1,90 @@
-#include"Graphics/Window.hpp"
 
-Graphics::Window window;
+#include"Graphics/Window.hpp"
+#include"Graphics/Image.hpp"
+#include"Graphics/Sprite.hpp"
+#include"Graphics/ResourceManager.hpp" //It includes font.hpp   *Transitive Includes*
+#include"Graphics/SpriteAnim.hpp"      //It includes spritesheet.hpp
+#include"Graphics/Timer.hpp"
+
+#include <fmt/core.h>
+
+#include <iostream>
+
+using namespace Graphics;
+
+Window window;
+Image image;
+Sprite sprite;
+
+const int SCREEN_WIDTH = 400;
+const int SCREEN_HEIGHT = 300;
 
 int main()
 {
-	window.create(L"Mini Assailants", 800, 600);
+	auto idleSprites = ResourceManager::loadSpriteSheet("assets/textures/Idle_Sheet.png", 153, 127, 0, 0, BlendMode::AlphaBlend);
+	SpriteAnim idleAnim(idleSprites,10.0f);
 
+	//Initialization Settings:
+	image.resize(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	window.create(L"Mini Assailants", SCREEN_WIDTH,SCREEN_HEIGHT);
 	window.show();
+	//window.setVSync(false);
+
+	Timer timer;
+	double      totalTime = 0.0;
+	uint64_t    frameCount = 0ull;
+	std::string fps = "FPS: 0";
 
 	while (window)
 	{
-		// Game Logic Here
+		timer.tick();
 
-		Graphics::Event event;
+		// Game Logic Here (Update Loop)
+
+		idleAnim.update(timer.elapsedSeconds());
+
+		image.clear(Color::Black);
+
+		// Draw Sprites here (Render Loop)
+
+		image.drawSprite(idleAnim, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+
+		image.drawText(Font::Default, fps, 10, 10, Color::White);
+
+		window.present(image);
+
+		Event event;
 		while (window.popEvent(event))
 		{
 			switch (event.type)
 			{
-			case Graphics::Event::Close:
+			case Event::Close:
 				window.destroy();
 				break;
-			case Graphics::Event::KeyPressed:
+			case Event::KeyPressed:
 			{
 				switch (event.key.code)
 				{
-				case Graphics::KeyCode::Escape:
+				case KeyCode::Escape:
 					window.destroy();
 					break;
 				}
 			}
 			}
+		}
+
+		++frameCount;
+
+		totalTime += timer.elapsedSeconds();
+		if (totalTime > 1.0)
+		{
+			fps = fmt::format("FPS: {:.3f}", static_cast<double>(frameCount) / totalTime);
+
+			std::cout << fps << std::endl;
+
+			frameCount = 0;
+			totalTime = 0.0;
 		}
 	}
 
